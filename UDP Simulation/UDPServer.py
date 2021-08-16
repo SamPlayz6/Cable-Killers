7bab9a7b2a5dee9510024fe19eb4ac9ba3dd15db
SamPlayz6 <44670760+SamPlayz6@users.noreply.github.com>
2021-08-16 13:58:55 +0100

    Adding time variable and got rid of frame resizing.

--- UDP Simulation/UDPServer.py
+++ UDP Simulation/UDPServer.py
@@ -8,7 +8,7 @@ BUFF_SIZE = 65536
 server_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 server_socket.setsockopt(socket.SOL_SOCKET,socket.SO_RCVBUF,BUFF_SIZE)
 host_name = socket.gethostname()
-host_ip = '192.168.11.105'#  socket.gethostbyname(host_name)
+host_ip = '192.168.43.207'#  socket.gethostbyname(host_name)
 print(host_ip)
 port = 9999
 socket_address = (host_ip,port)
@@ -18,13 +18,18 @@ print('Listening at:',socket_address)
 vid = cv2.VideoCapture(0) #  replace 'rocket.mp4' with 0 for webcam
 fps,st,frames_to_count,cnt = (0,0,20,0)
 
+#time
+t = time.time()
+#print(t)
+
 while True:
 	msg,client_addr = server_socket.recvfrom(BUFF_SIZE)
+	#print(t)
 	print('GOT connection from ',client_addr)
 	WIDTH=400
 	while(vid.isOpened()):
 		_,frame = vid.read()
-		frame = imutils.resize(frame,width=WIDTH)
+		#frame = imutils.resize(frame,width=WIDTH)
 		encoded,buffer = cv2.imencode('.jpg',frame,[cv2.IMWRITE_JPEG_QUALITY,80])
 		message = base64.b64encode(buffer)
 		server_socket.sendto(message,client_addr)
